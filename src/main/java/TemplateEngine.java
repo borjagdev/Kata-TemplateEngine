e96f28de20839b08b1710b247ea2baaf7216ff4a
Borja García Glez <alu0100897562@ull.edu.es>
2020-06-11 16:30:01 +0100

    First Test: Return same input when no variables are given

--- src/main/java/TemplateEngine.java
+++ src/main/java/TemplateEngine.java
@@ -1,5 +1,7 @@
+import java.util.HashMap;
+
 public class TemplateEngine {
-    public static String parse(String s) {
-        return "";
+    public static String parse(String template, HashMap<String, String> values) {
+        return template;
     }
 }
--- src/test/java/TemplateEngineShould.java
+++ src/test/java/TemplateEngineShould.java
@@ -1,11 +1,22 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TemplateEngineShould {
 
+    /* TO DO:
+    - ("hola", {}) -> "hola"
+    - ("hola `$user`", {}) -> "hola `$user`"
+    - ("hola `$name`", {user=Carlos}) -> "hola `$name`"
+    - ("hola `$user`, hoy es `$day`", {user=Carlos, day=miercoles}) -> "hola Carlos, hoy es miercoles"
+    */
+
     @Test
-    public void test() {
-        assertThat(TemplateEngine.parse("")).isEqualTo("");
+    void return_same_input_when_no_variables_are_given() {
+        HashMap<String,String> variables = new HashMap<>();
+        assertThat(TemplateEngine.parse("Hola", variables)).isEqualTo("Hola");
     }
+
 }
