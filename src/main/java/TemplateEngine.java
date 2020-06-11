601f96c78f7ccf3de02c0132748a0a55dd6375fa
Borja García Glez <alu0100897562@ull.edu.es>
2020-06-11 16:45:41 +0100

    Second Test: Parse template and substitute a variable

--- src/main/java/TemplateEngine.java
+++ src/main/java/TemplateEngine.java
@@ -1,7 +1,13 @@
 import java.util.HashMap;
 
 public class TemplateEngine {
-    public static String parse(String template, HashMap<String, String> values) {
+    public static String parse(String template, HashMap<String, String> variables) {
+        if (!variables.isEmpty()) {
+            String key = variables.entrySet().iterator().next().getKey();
+            String value = variables.get(key);
+            String templateVariable = "`$" + key + "`";
+            template = template.replace(templateVariable, value);
+        }
         return template;
     }
 }
--- src/test/java/TemplateEngineShould.java
+++ src/test/java/TemplateEngineShould.java
@@ -9,14 +9,22 @@ public class TemplateEngineShould {
     /* TO DO:
     - ("hola", {}) -> "hola"
     - ("hola `$user`", {}) -> "hola `$user`"
-    - ("hola `$name`", {user=Carlos}) -> "hola `$name`"
+    - ("hola `$name`", {user=Carlos}) -> "hola Carlos"
     - ("hola `$user`, hoy es `$day`", {user=Carlos, day=miercoles}) -> "hola Carlos, hoy es miercoles"
     */
 
     @Test
     void return_same_input_when_no_variables_are_given() {
-        HashMap<String,String> variables = new HashMap<>();
+        HashMap<String, String> variables = new HashMap<>();
         assertThat(TemplateEngine.parse("Hola", variables)).isEqualTo("Hola");
+        assertThat(TemplateEngine.parse("Hola `$user`", variables)).isEqualTo("Hola `$user`");
+    }
+
+    @Test
+    void parse_template_and_substitute_a_variable() {
+        HashMap<String, String> variables = new HashMap<>();
+        variables.put("user", "Carlos");
+        assertThat(TemplateEngine.parse("Hola `$user`", variables)).isEqualTo("Hola Carlos");
     }
 
 }
