ffdca6060df021ebd93f1535b5d32c08d8e2dd1d
Borja García Glez <alu0100897562@ull.edu.es>
2020-06-11 17:19:27 +0100

    Refactor: From While to For loop

--- src/main/java/TemplateEngine.java
+++ src/main/java/TemplateEngine.java
@@ -1,20 +1,12 @@
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class TemplateEngine {
     public static String parse(String template, HashMap<String, String> variables) {
-        Iterator<Map.Entry<String, String>> iterator = variables.entrySet().iterator();
-        String key;
-        String value;
-        String templateVariable;
-        while (iterator.hasNext()) {
-            key = iterator.next().getKey();
-            value = variables.get(key);
-            templateVariable = "`$" + key + "`";
-            template = template.replace(templateVariable, value);
+        for (Map.Entry<String, String> pair : variables.entrySet()) {
+            String templateVariable = "`$" + pair.getKey() + "`";
+            template = template.replace(templateVariable, pair.getValue());
         }
-
         return template;
     }
 }
